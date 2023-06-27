ce79d0f045c3822069fbd8900b15abe37619d361
Lennon Araujo <lennon_hih@hotmail.com>
2023-06-27 19:15:01 -0300

    Finish

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Model, createServer } from 'miragejs';
 import { App } from './App';
-import { request } from 'http';
 
 createServer({
   models: {
@@ -11,24 +10,7 @@ createServer({
 
   seeds(server) {
     server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Freelance de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2021-02-12 09:00:00'),
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 2000,
-          createdAt: new Date('2021-02-20 09:00:00'),
-        }
-      ],
+      transactions: [],
     })
   },
 
